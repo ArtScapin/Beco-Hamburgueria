d5b654740b94d1c39fed89f73ba360bc49eecbcd
Arthur Scapin Pereira <106567677+ArtScapin@users.noreply.github.com>
2022-08-01 01:25:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,111 +1,117 @@
-function irCarrinho(opcao){
-	window.location.href = "secoes/pedidos.html?opcao="+String(opcao);
-}
+let arquivo = window.location.href;
+arquivo = arquivo.split('/');
+arquivo = arquivo[arquivo.length-1]
+arquivo = arquivo.split('.');
+arquivo = arquivo[0];
 
-const urlParams = new URLSearchParams(window.location.search);
-let pedido = typeof(urlParams.get('opcao')) !== 'string' ? 1 : urlParams.get('opcao');
-let valor = 0;
-let quantidade = 1;
-let opAtual = 1;
+if (arquivo === 'index') {
+	mobileTransition();
 
-if(document.getElementById("pedido"+String(pedido))!==null){
-	document.getElementById("pedido"+String(pedido)).checked=true;
+	function mobileTransition(){
+		if(innerWidth<=1120){
+			$("#cardsRow1").hide();
+			$("#cardsRow2").hide();
+			$("#listaRow1").show();
+			$("#listaRow2").show();
+		}else{
+			$("#cardsRow1").show();
+			$("#cardsRow2").show();
+			$("#listaRow1").hide();
+			$("#listaRow2").hide();
+		}
+		if(innerWidth<=650){
+			$("#carouselRow").hide();
+		}else{
+			$("#carouselRow").show();
+		}
+		alterarNome();
+	}
+
+	function irCarrinho(opcao){
+		window.location.href = "secoes/pedidos.html?opcao="+String(opcao);
+	}
+
+
+
+}else if (arquivo === 'pedidos'){
+	const urlParams = new URLSearchParams(window.location.search);
+	let pedido = typeof(urlParams.get('opcao')) !== 'string' ? 1 : urlParams.get('opcao');
+	let valor = 0;
+	let quantidade = 1;
+	let opAtual = 1;
+
+	alterarNome();
 	atualizarPedido(parseInt(pedido));
-	alterarNome();
-}else{
-	removeOculto();
-}
 
+	function atualizarPedido(opcao){
+		opAtual=opcao;
+		switch(opcao){
+			case 1:
+				valor = 22;
+				document.getElementById("name_burguer").innerHTML="Beco Diagonal";
+				break;
+			case 2:
+				valor = 20;
+				document.getElementById("name_burguer").innerHTML="Super Burguer";
+				break;
+			case 3:
+				valor = 30;
+				document.getElementById("name_burguer").innerHTML="Bacon 360";
+				break;
+			case 4:
+				valor = 37;
+				document.getElementById("name_burguer").innerHTML="Combo Diagonal";
+				break;
+			case 5:
+				valor = 35;
+				document.getElementById("name_burguer").innerHTML="Super Combo";
+				break;
+			case 6:
+				valor = 45;
+				document.getElementById("name_burguer").innerHTML="Combo 360";
+				break;
+		}
+		document.getElementById("pedido"+String(opAtual)).checked=true;
+		document.getElementById("valor_burguer").innerHTML = "R$"+String(valor)+",00"; 
+		document.getElementById("qtde_burguer").innerHTML=String(quantidade);
+		document.getElementById("subtotal").innerHTML = "R$"+String(valor*quantidade)+",00"; 
+		document.getElementById("total").innerHTML = "R$"+String(valor*quantidade)+",00";
+	}
 
-function atualizarPedido(opcao){
-	opAtual=opcao;
-	switch(opcao){
-		case 1:
-			valor = 22;
-			document.getElementById("name_burguer").innerHTML="Beco Diagonal";
-			break;
-		case 2:
-			valor = 20;
-			document.getElementById("name_burguer").innerHTML="Super Burguer";
-			break;
-		case 3:
-			valor = 30;
-			document.getElementById("name_burguer").innerHTML="Bacon 360";
-			break;
-		case 4:
-			valor = 37;
-			document.getElementById("name_burguer").innerHTML="Combo Diagonal";
-			break;
-		case 5:
-			valor = 35;
-			document.getElementById("name_burguer").innerHTML="Super Combo";
-			break;
-		case 6:
-			valor = 45;
-			document.getElementById("name_burguer").innerHTML="Combo 360";
-			break;
+	function atualizarQtde(incremento){
+		if((parseInt(quantidade)+parseInt(incremento))>=0){
+			quantidade=parseInt(quantidade)+parseInt(incremento);
+			atualizarPedido(opAtual);
+			document.getElementById("qtde").innerHTML = quantidade;
+		}
 	}
-	document.getElementById("pedido"+String(opAtual)).checked=true;
-	document.getElementById("valor_burguer").innerHTML = "R$"+String(valor)+",00"; 
-	document.getElementById("qtde_burguer").innerHTML=String(quantidade);
-	document.getElementById("subtotal").innerHTML = "R$"+String(valor*quantidade)+",00"; 
-	document.getElementById("total").innerHTML = "R$"+String(valor*quantidade)+",00";
-}
 
-function atualizarQtde(incremento){
-	if((parseInt(quantidade)+parseInt(incremento))>=0){
-		quantidade=parseInt(quantidade)+parseInt(incremento);
-		atualizarPedido(opAtual);
-		document.getElementById("qtde").innerHTML = quantidade;
-	}
-}
-
-function enviarPedido(){
-	if(document.getElementById("nome").value.length >= 7 && document.getElementById("endereco").value.length >= 10 && document.getElementById("numero").value.length >= 8){
-		alert(`${document.getElementById("nome").value},\nSeu pedido será enviado para:\n- ${document.getElementById("endereco").value}\n\nEm caso de problemas com seu pedido entraremos em contato através do número : ${document.getElementById("numero").value}`);
-		window.location.href = '../index.html'
-	}else{
-		if(document.getElementById("nome").value.length >= 7 && document.getElementById("endereco").value.length >= 10)
-			alert('Você esqueceu de nos dizer seu número!');
-		else if(document.getElementById("nome").value.length >= 7 && document.getElementById("numero").value.length >= 8)
-			alert('Você esqueceu de nos dizer seu endereço!');
-		else if(document.getElementById("endereco").value.length >= 10 && document.getElementById("numero").value.length >= 8)
-			alert('Você esqueceu de nos dizer seu nome completo!');
-		else if(document.getElementById("nome").value.length >= 7)
-			alert('Você esqueceu de nos dizer seu endereço e seu número!');
-		else if(document.getElementById("endereco").value.length >= 10)
-			alert('Você esqueceu de nos dizer seu nome e seu número!');
-		else if(document.getElementById("numero").value.length >= 8)
-			alert('Você esqueceu de nos dizer seu nome completo e seu endereço!');
-		else
-			alert('Você esqueceu de nos dizer seus dados para a entrega!');
-	}
-}
-
-function removeOculto(){
-	if(innerWidth<=1120){
-		$("#ocultar1").hide();
-		$("#ocultar2").hide();
-		$("#ocultar3").show();
-		$("#ocultar4").show();
-	}else{
-		$("#ocultar1").show();
-		$("#ocultar2").show();
-		$("#ocultar3").hide();
-		$("#ocultar4").hide();
-	}
-	if(innerWidth<=650){
-		$("#carousel").hide();
-	}else{
-		$("#carousel").show();
-	}
-	alterarNome();
+	function enviarPedido(){
+		if(document.getElementById("nome").value.length >= 7 && document.getElementById("endereco").value.length >= 10 && document.getElementById("numero").value.length >= 8){
+			alert(`${document.getElementById("nome").value},\nSeu pedido será enviado para:\n- ${document.getElementById("endereco").value}\n\nEm caso de problemas com seu pedido entraremos em contato através do número : ${document.getElementById("numero").value}`);
+			window.location.href = '../index.html'
+		}else{
+			if(document.getElementById("nome").value.length >= 7 && document.getElementById("endereco").value.length >= 10)
+				alert('Você esqueceu de nos dizer seu número!');
+			else if(document.getElementById("nome").value.length >= 7 && document.getElementById("numero").value.length >= 8)
+				alert('Você esqueceu de nos dizer seu endereço!');
+			else if(document.getElementById("endereco").value.length >= 10 && document.getElementById("numero").value.length >= 8)
+				alert('Você esqueceu de nos dizer seu nome completo!');
+			else if(document.getElementById("nome").value.length >= 7)
+				alert('Você esqueceu de nos dizer seu endereço e seu número!');
+			else if(document.getElementById("endereco").value.length >= 10)
+				alert('Você esqueceu de nos dizer seu nome e seu número!');
+			else if(document.getElementById("numero").value.length >= 8)
+				alert('Você esqueceu de nos dizer seu nome completo e seu endereço!');
+			else
+				alert('Você esqueceu de nos dizer seus dados para a entrega!');
+		}
+	}	
 }
 
 function alterarNome(){
-	if(innerWidth<=650){
-		document.getElementById("nomeEmpresa").innerHTML = "Beco"
-	}else{
-		document.getElementById("nomeEmpresa").innerHTML = "Beco Hamburgueria"
-	}
+	if(innerWidth<=650)
+		document.getElementById("nomeHamburgueria").innerHTML = "Beco"
+	else
+		document.getElementById("nomeHamburgueria").innerHTML = "Beco Hamburgueria"
 }
